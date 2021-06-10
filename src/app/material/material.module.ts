4407653c8ddd87dbda62bd6f22f5717efbf1dabe
Dhereck1 <82067086+Dhereck1@users.noreply.github.com>
2021-06-10 01:57:18 -0400

    add: visual login

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button'
+import {MatCardModule} from '@angular/material/card';
 
 const MaterialComponents =[
   MatToolbarModule,
   MatIconModule,
-  MatFormFieldModule
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule
 ]
 
 
