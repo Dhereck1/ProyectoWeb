9f0666bd8e828331306c3d0c12b0afcdd848b202
Demitoo <76886434+Demitoo@users.noreply.github.com>
2021-06-12 23:32:02 -0400

    actulizacion de servicioPaciente y vista historiaclinica

--- src/app/servicios/paciente.service.ts
+++ src/app/servicios/paciente.service.ts
@@ -1,24 +1,30 @@
 import { Injectable } from '@angular/core';
 import { paciente } from '../interfaces/interfaces';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class PacienteService {
   lista: paciente[] = [];
-  constructor() { }
+  servidor= "http://localhost:3500";
+  constructor(private servicio:HttpClient) { }
 
 
-  addPaciente(cliente:paciente){
-    this.lista.push(cliente);
+  addPaciente(cliente:paciente):Observable<any>{
+    console.log(cliente)
+    return this.servicio.post(`${this.servidor}/api/paciente/registro`,cliente)
   }
-
-  getPaciente(){
-    return this.lista;
-  }
-
   setPacientes(lista:paciente[]){
     this.lista=lista;
+  }
+  getPaciente(id:number):Observable<any>{
+    return this.servicio.get(`${this.servidor}/api/paciente/${id}`);
+  }
+  getHistoria(id:number):Observable<any>{
+    return this.servicio.get(`${this.servidor}/api/paciente/${id}/historia`);
   }
 
 
