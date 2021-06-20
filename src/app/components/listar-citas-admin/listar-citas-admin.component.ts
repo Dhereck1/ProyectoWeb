1548ecbad3eecb25e11f4ee88ce28b1d0b820c51
Demitoo <76886434+Demitoo@users.noreply.github.com>
2021-06-19 21:31:26 -0400

    act

--- src/app/components/listar-citas-admin/listar-citas-admin.component.ts
+++ src/app/components/listar-citas-admin/listar-citas-admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { cita, medico } from '../../interfaces/interfaces';
+import { PacienteService } from 'src/app/servicios/paciente.service';
+import { cita, medico, paciente } from '../../interfaces/interfaces';
 import { ConexionService } from '../../servicios/conexion.service';
 
 @Component({
@@ -10,16 +11,32 @@ import { ConexionService } from '../../servicios/conexion.service';
 })
 export class ListarCitasAdminComponent implements OnInit {
   citas: Array<cita> = [];
+  citasfiltradas: Array<cita> = [];
   abierta: Array<cita> = [];
   asignada: Array<cita> = [];
   cerrada: Array<cita> = [];
   nombresMedico: Array<medico> = [];
+  pacientes: Array<paciente> = [];
+  medicos: Array<medico> = [];
+  filtroPaciente:number
+  filtroMedico:number
+  filtroEstado:string
+  inicial=""
 
-  constructor(private servicioDatos: ConexionService, private router: Router) {}
+  constructor(private servicioDatos: ConexionService, private router: Router, private servicePaciente:PacienteService) {}
 
   ngOnInit(): void {
+
+    this.servicePaciente.getAllPacientes().subscribe(datos=>{
+      this.pacientes=datos;
+    });
+    this.servicioDatos.getAllMeds().subscribe(datos=>{
+      this.medicos=datos;
+    })
+
     this.servicioDatos.getAllCitas().subscribe((res) => {
       this.citas = res;
+      this.citasfiltradas=res
 
         this.citas.forEach((cita) => {
         if (cita.idMedico) {
@@ -45,6 +62,36 @@ export class ListarCitasAdminComponent implements OnInit {
   //hacer componente de form cita
   editarCita(id: any) {
     this.router.navigate([`admin/formCitaAdmi/${id}`]);
+  }
+  appFiltro(){
+    /*console.log(this.filtroEstado)
+    console.log(this.filtroMedico)
+    console.log(this.filtroPaciente)*/
+    this.citasfiltradas=this.citas
+    if(this.filtroPaciente!=null)
+    this.citasfiltradas=this.citasfiltradas.filter((cita)=>cita.idUsuario===this.filtroPaciente)
+    if(this.filtroMedico!=null)
+    this.citasfiltradas=this.citasfiltradas.filter((cita)=>cita.idMedico===this.filtroMedico)
+    if(this.filtroEstado!=null)
+    this.citasfiltradas=this.citasfiltradas.filter((cita)=>cita.estado===this.filtroEstado)
+    this.abierta = this.citasfiltradas.filter((cita) => cita.estado === 'Abierta');
+    this.asignada = this.citasfiltradas.filter((cita) => cita.estado === 'Asignada');
+    this.cerrada = this.citasfiltradas.filter((cita) => cita.estado === 'Cerrada');
+  }
+  setPac(id:number){
+    this.filtroPaciente=id
+  }
+  setMed(id:number){
+    this.filtroMedico=id
+  }
+  setEst(estado:string){
+    this.filtroEstado=estado
+  }
+  reset(){
+    this.abierta = this.citas.filter((cita) => cita.estado === 'Abierta');
+    this.asignada = this.citas.filter((cita) => cita.estado === 'Asignada');
+    this.cerrada = this.citas.filter((cita) => cita.estado === 'Cerrada');
+    this.inicial=""
   }
 }
 
