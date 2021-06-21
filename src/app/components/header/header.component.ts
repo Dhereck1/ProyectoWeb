ce8a585b5c5dd7e6a4b9d1105cd6a6edb041b896
jo <81889339+Josefina-db@users.noreply.github.com>
2021-06-21 12:55:47 -0400

    estilos2
    nuevas cosillas de estilo y ruta

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StorageService } from 'src/app/servicios/storage.service';
 import { ConexionService } from '../../servicios/conexion.service';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
